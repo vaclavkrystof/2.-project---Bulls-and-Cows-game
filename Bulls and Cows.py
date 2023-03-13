be291a8d08d056107a1cb339edaf78244ac4e120
Vaclav Krystof <v.krystof@seznam.cz>
2023-03-13 20:37:56 +0100

    Final

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -7,7 +7,8 @@ discord: Vašek K.#0340
 
 import random
 
-print("""
+def hra():
+    print("""
 Hi there!
 -------------------------------------------------------------
 I've generated a random 4 digit number for you.
@@ -15,69 +16,67 @@ Guess a secret 4-digit number (except "0") with unique values
 Let's play a bulls and cows game.
 -------------------------------------------------------------""")
 
-def nahodne_cislo(cisla: list):
-    """
-    Creates a unique 4-digit number from list of numbers
-    """
-    return random.sample(cisla, k=4)
+    cisla = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    random_num = random.sample(cisla, k=4)
+    pocet_kol = 0
 
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-random_num = nahodne_cislo(a)
-pocet_kol = 0
-
-while True:
-    akce = input("Zadej 4-místné číslo od 1-9, čísla se nesmí opakovat:")  # creates string
-            
-    if akce[0] == "0" or len(set(akce)) != 4 or not akce.isdigit():
-        print(f"Zadáno: {akce}")
-        print("Musí obsahovat jen čísla, nesmí obsahovat 0, nesmí být kratší/delší než 4 znaky")
-        continue
-
-    list_akce = [int(i) for i in akce]
-            
-    if list_akce == random_num:
-        pocet_kol += 1
-        print(f"Correct, you've guessed the right number: {akce}")
-        print(f"Guessed in {pocet_kol} guesses!")
-        print("-------------------------------------------------------------")
-        break
-
-    else:
-        bull = []
-        cow = []
-        for x in range(0,4):
-            if list_akce[x] == random_num[x]:
-                bull.append((list_akce[x]))
-            elif list_akce[x] not in bull and list_akce[x] in random_num:
-                cow.append((list_akce[x]))
-        pocet_kol += 1
+    while True:
+        akce = input("Zadej 4-místné číslo od 1-9, čísla se nesmí opakovat:")  # creates string
                 
-    if len(bull) == 0 and len(cow) == 0:
-        print("Bulls:", len(bull) , "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) == 0 and len(cow) == 1:
-        print("Bulls:", len(bull), "Cow:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) == 0 and len(cow) > 1:
-        print("Bulls:", len(bull), "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) == 1 and len(cow) == 1:
-        print("Bull:", len(bull), "Cow:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) == 1 and len(cow) == 0:
-        print("Bull:", len(bull), "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) == 1 and len(cow) > 1:
-        print("Bull:", len(bull), "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) > 1 and len(cow) == 0:
-        print("Bulls:", len(bull), "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) > 1 and len(cow) == 1:
-        print("Bulls:", len(bull), "Cow:", len(cow))
-        print("-------------------------------------------------------------")
-    elif len(bull) > 1 and len(cow) > 1:
-        print("Bulls:", len(bull), "Cows:", len(cow))
-        print("-------------------------------------------------------------")
-    bull.clear()
-    cow.clear()
+        if akce[0] == "0" or len(set(akce)) != 4 or not akce.isdigit():
+            print(f"Zadáno: {akce}")
+            print("Musí obsahovat jen čísla, nesmí obsahovat 0, nesmí být kratší/delší než 4 znaky")
+            print("-------------------------------------------------------------")
+            continue
+
+        list_akce = [int(i) for i in akce]
+                
+        if list_akce == random_num:
+            pocet_kol += 1
+            print(f"Correct, you've guessed the right number: {akce}")
+            print(f"Guessed in {pocet_kol} guesses!")
+            print("-------------------------------------------------------------")
+            break
+
+        else:
+            bull = []
+            cow = []
+            for x in range(0,4):
+                if list_akce[x] == random_num[x]:
+                    bull.append((list_akce[x]))
+                elif list_akce[x] not in bull and list_akce[x] in random_num:
+                    cow.append((list_akce[x]))
+            pocet_kol += 1
+                    
+        if len(bull) == 0 and len(cow) == 0:
+            print("Bulls:", len(bull) , "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) == 0 and len(cow) == 1:
+            print("Bulls:", len(bull), "Cow:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) == 0 and len(cow) > 1:
+            print("Bulls:", len(bull), "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) == 1 and len(cow) == 1:
+            print("Bull:", len(bull), "Cow:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) == 1 and len(cow) == 0:
+            print("Bull:", len(bull), "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) == 1 and len(cow) > 1:
+            print("Bull:", len(bull), "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) > 1 and len(cow) == 0:
+            print("Bulls:", len(bull), "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) > 1 and len(cow) == 1:
+            print("Bulls:", len(bull), "Cow:", len(cow))
+            print("-------------------------------------------------------------")
+        elif len(bull) > 1 and len(cow) > 1:
+            print("Bulls:", len(bull), "Cows:", len(cow))
+            print("-------------------------------------------------------------")
+        bull.clear()
+        cow.clear()
+
+if __name__ == "__main__":
+    hra()
